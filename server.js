bb92668321d807ec0c622781c6f60b31d4c7c92e
Yuri Araújo <araujoyuri@edu.unifor.br>
2019-04-11 14:16:28 -0300

    Added server for heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+let serveStatic = require('serve-static')
+
+let app = express()
+app.use(serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log('Server started at ', port)

d4254abcf02fbcc92053611c9c4b0bcf8e100764
Yuri Araújo <araujoyuri@edu.unifor.br>
2019-04-30 10:06:10 -0300

    Added active user on toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,19 +1,29 @@
 <template>
-  <v-toolbar color="indigo" dark fixed app>
-    <v-toolbar-title>Twitter DevEngers</v-toolbar-title>
-    <v-toolbar-items>
-      <v-btn to="/" flat>Página Inicial</v-btn>
-      <v-btn v-if="isAuthenticated" @click="logout" flat>Logout</v-btn>
-      <v-btn v-if="isAuthenticated" fab small color="blue accent-2" bottom right absolute @click="add_tweet = true">
-        <v-icon>fas fa-plus-circle</v-icon>
-      </v-btn>
-      <v-dialog v-model="add_tweet" max-width="600px">
-        <add-tweet @close="add_tweet = false"/>
-      </v-dialog>
-    </v-toolbar-items>
-    <v-spacer></v-spacer>
-    <v-btn v-if="active_user">{{ active_user.login }}</v-btn>
-  </v-toolbar>
+  <v-card flat>
+    <v-container fluid>
+      <v-layout row child-flex wrap>
+        <div>
+          <v-toolbar color="indigo" dark fixed app>
+            <v-toolbar-title>Twitter DevEngers</v-toolbar-title>
+            <v-toolbar-items>
+              <v-btn to="/" flat>Página Inicial</v-btn>
+              <v-btn v-if="isAuthenticated" @click="logout" flat>Logout</v-btn>
+              <v-btn v-if="isAuthenticated" fab small color="blue accent-2" bottom right absolute @click="add_tweet = true">
+                <v-icon>fas fa-plus-circle</v-icon>
+              </v-btn>
+              <v-dialog v-model="add_tweet" max-width="600px">
+                <add-tweet @close="add_tweet = false"/>
+              </v-dialog>
+              <v-spacer></v-spacer>
+              <v-btn color="blue" v-if="activeUser">
+                <v-icon>fas fa-user-circle</v-icon>&nbsp;{{ activeUser }}
+              </v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+        </div>
+      </v-layout>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
